1cb0ab6e632c56d69923032682c8e5581c178024
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-06-28 00:33:42 +0500

    Add todo to contact fetch method

--- client/contact.py
+++ client/contact.py
@@ -7,4 +7,6 @@ class Contact:
     def fetch(self, contact_id):
         url = 'contacts/{id}'.format(id=contact_id)
 
+        # todo: return an object to be able to do this: contact = client.contact.fetch('123131') \n contact.get('name')
+        # todo: this object can also return a json representation of its data: contact.json()
         return self.requester.get(url)
