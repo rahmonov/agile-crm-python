452758d4a49bee592a2d719ee49a8ce7165304fe
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-07-01 00:43:43 +0500

    Implement company creating

--- client/company.py
+++ client/company.py
@@ -4,6 +4,14 @@ class Company:
     def __init__(self, requester):
         self.requester = requester
 
+    def create(self, company_data):
+        url = 'contacts'
+
+        if 'type' not in company_data:
+            company_data['type'] = 'COMPANY'
+
+        return self.requester.post(url, company_data)
+
     def fetch(self, company_id):
         url = 'contacts/{id}'.format(id=company_id)
 
