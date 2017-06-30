0c6d4c1f57d154978c39a69c61201f1b85140587
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-07-01 01:04:42 +0500

    Implement updating and fetching a deal

--- client/deal.py
+++ client/deal.py
@@ -8,3 +8,15 @@ class Deal:
         url = 'opportunity'
 
         return self.requester.post(url, deal_data)
+
+    def update(self, deal_id, deal_data):
+        url = 'opportunity/partial-update'
+
+        deal_data['id'] = deal_id
+
+        return self.requester.put(url, deal_data)
+
+    def fetch(self, deal_id):
+        url = 'opportunity/{deal_id}'.format(deal_id=deal_id)
+
+        return self.requester.get(url)
