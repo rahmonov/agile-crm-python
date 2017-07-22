996ed7cde395c722ef29821b10f9a83340be19e2
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-07-23 00:02:37 +0500

    Prepare setup files and license

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Agile CRM
 
 A Python interface for the Agile CRM API.
 
-This library is being made because the official Agile CRM Python API is not good, to say the least.
+
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='agile-crm-python',
+  packages=['agilecrm'],
+  version='0.1',
+  description='Python wrapper for Agile CRM API',
+  author='Jahongir Rahmonov',
+  author_email='jrahmonov2@gmail.com',
+  url='https://github.com/rahmonov/agile-crm-python',
+  download_url='https://github.com/rahmonov/agile-crm-python/archive/0.1.tar.gz',
+  keywords=['python', 'api', 'agilecrm', 'crm', 'rest'],
+  classifiers=[],
+)
